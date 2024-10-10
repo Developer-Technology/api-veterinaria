e704317aa3fc4ca21fe582e1b01dc9fb4caa378e
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-10-09 20:40:45 -0500

    Login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('doc')->nullable();
             $table->string('name');
+            $table->string('last_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->text('photo')->nullable();
+            $table->string('username')->nullable();
             $table->string('password');
+            $table->string('status')->nullable();
+            $table->string('privilege')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -29,4 +37,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
+
 };
